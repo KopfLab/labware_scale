2dbffd7b6c19042e282562b9625eeb1c6e90c142
Sebastian Kopf <seb.kopf@gmail.com>
2018-07-01 23:07:32 -0600

    fix typo

--- ScaleState.h
+++ ScaleState.h
@@ -23,7 +23,7 @@ struct ScaleState : public DeviceStateSerial {
 
   ScaleState() {};
 
-  ScaleState (bool locked, bool state_logging, bool data_logging, uint data_reading_period_min, uint data_reading_period, uint data_logging_period, uint8_t data_logging_type, uint calc_ate) :
+  ScaleState (bool locked, bool state_logging, bool data_logging, uint data_reading_period_min, uint data_reading_period, uint data_logging_period, uint8_t data_logging_type, uint calc_rate) :
     DeviceStateSerial(locked, state_logging, data_logging, data_reading_period_min, data_reading_period, data_logging_period, data_logging_type), calc_rate(calc_rate) {};
 
 };
